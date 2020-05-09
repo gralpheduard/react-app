972df58a1eb48f2900c799a8f1de18273c5d1f46
unknown <gralpheduard@gmail.com>
2020-05-09 18:12:56 +0800

    Updates

---
+++ precache-manifest.4e7ff129ba14ab9cf5aeedc79810a829.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe10640032dc0dadcfef7bf94feafaec",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "e5475fd2f32009ef0716",
+    "url": "/react-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "673f23783b8d428ec556",
+    "url": "/react-app/static/js/2.eb8bda40.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app/static/js/2.eb8bda40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5475fd2f32009ef0716",
+    "url": "/react-app/static/js/main.edcff485.chunk.js"
+  },
+  {
+    "revision": "f71561f3f54bb8d5a715",
+    "url": "/react-app/static/js/runtime-main.559b46ab.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
