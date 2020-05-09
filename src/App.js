2c8128a0c238fe1bed7d3c46f2dd3588b6e261c5
unknown <gralpheduard@gmail.com>
2020-05-09 23:07:43 +0800

    add navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
 function App() {
+  const classes = useStyles();
   return (
     <div className="App">
+    <AppBar position="static"className={classes.root}>
+      <Toolbar>
+        <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" className={classes.title}>
+          News
+        </Typography>
+        <Button color="inherit">Login</Button>
+      </Toolbar>
+    </AppBar>
       <h1>React App</h1>
       <h2>Author: Ralph Eduard C. Gannaban</h2>
       <header className="App-header">
@@ -21,9 +51,7 @@ function App() {
         >
           Learn React
         </a>
-        <Button variant="contained" color="primary">
-          Hello World
-        </Button>
+
       </header>
     </div>
   );
